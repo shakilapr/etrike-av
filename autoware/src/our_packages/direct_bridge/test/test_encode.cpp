6e690c7b9f28b1496ecfcf7c07a6d7b9ff8d5a88
shakilapr <shakilabeta@gmail.com>
2026-08-31 15:35:40 +0530

    build(bridge): lint only source dirs; fix test include order

--- autoware/src/our_packages/direct_bridge/test/test_encode.cpp
+++ autoware/src/our_packages/direct_bridge/test/test_encode.cpp
@@ -12,12 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <linux/can.h>
+
 #include <cassert>
 #include <cmath>
 #include <cstdint>
 #include <cstring>
-
-#include <linux/can.h>
 
 #include "direct_bridge/direct_bridge_node.hpp"
 #include "protocol/codecs/seb.hpp"
